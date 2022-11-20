92a4c8dc056f285803aec4f41367a3b3f0b24bdd
kohtaroh17 <117917719+kohtaroh17@users.noreply.github.com>
2022-11-20 13:12:12 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,61 @@
+class BooksController < ApplicationController
+  
+  def new
+    @book = Book.new
+  end 
+  
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    if @book.save
+      flash[:notice] = "successfully" 
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render "index"
+    end
+    
+  end 
+
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def destroy 
+    @book = Book.find(params[:id])
+    @book.destroy
+    if flash[:notice] = "successfully" 
+     redirect_to '/books'
+    else
+      flash.now[:alert] = "error"
+      render "index"
+    end
+  end 
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "error"
+      render "edit"
+    end
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end 
+  
+  
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
